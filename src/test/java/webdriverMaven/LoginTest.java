01daa11b70d7237e01f3adf48c4b9a38390c3a8c
Easy2Automate <t.sulagna@gmail.com>
2020-02-25 16:47:11 +0530

    Innitial Commit

---
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -0,0 +1,33 @@
+package webdriverMaven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest
+{
+ WebDriver Driver;
+@BeforeSuite	
+public void Setup()
+{
+	System.setProperty("webdriver.chrome.driver", "D:\\SeleniumPractice\\chromedriver_79\\chromedriver.exe");
+	Driver=new ChromeDriver();
+	Driver.manage().window().maximize();
+	
+}
+@Test
+public void DoLogin()
+{
+	Driver.get("https://www.amazon.in/");
+	Driver.findElement(By.xpath("//*[@id=\'nav-signin-tooltip\']/a/span")).click();
+	
+}
+@AfterSuite
+public void TearDown()
+{
+	Driver.quit();
+}
+}
